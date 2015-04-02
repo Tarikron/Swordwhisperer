7aa9e218b3b201c0ae6f5c03c5bce2f4944fc199
MaSieber <marco.sieber@games-academy.de>
2015-04-02 18:09:14 +0200

    Prototype Branch
    *create prototype branch?
    * added first prototype attempt

---
+++ Assets/scripts/cPlayer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var fSpeed : float = 2.0f;
+
+function Start () {
+
+}
+
+function Update () {
+
+
+	if (Input.GetButton("Horizontal"))
+	{
+ 		this.transform.position.x += fSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+ 	}
+
+}
