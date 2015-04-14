cf0a8dbf96396d1048604665d478042f350c4cfa
MaSieber <marco.sieber@games-academy.de>
2015-04-14 17:41:03 +0200

    tääähhsst, kommt alles an?
    *added turtle, which wakes up if you come near
    *added prototype attack, turtle disappears after that

--- Assets/scripts/cPlayer_c.cs
+++ Assets/scripts/cPlayer_c.cs
@@ -6,32 +6,42 @@ using System.Collections;
 public class cPlayer_c : MonoBehaviour 
 {
 
+	//settings via unity
 	public SkeletonAnimation skeletonAnimation;
-	public float jumpForce = 300f;
+	public float jumpHeight = 300f;
+	public float jumpTime = 10.0f;
 	public float walkVelocity = 3.0f;
+	public float gravity = 1.0f;
 
+	//animation stuff
 	private bool animIsPlaying = false;
 	private bool animAdd = false; //Test var
+	private bool isAnimQueueDone = false;
 	private bool animLoop = false;
 	private string currentAnimation = "";
 	private string animationToPlay = "idle_nosword";
 	private string animationToPlayAfter = "idle_nosword";
-	private bool bCollectedSword = false;
 
+	//gameplay related
+	private bool bCollectedSword = false;
+	private bool bGrounded = false;
+	private int iJumpCounter = 0;
+	private Vector2 movementDirection;
+	private Rigidbody2D rb2D;
+	private float jumpDestHeight = 0.0f;
 
 	void collectSword()
 	{
 
 	}
 
-	void handleMovement()
+	void handleMovement(float x)
 	{
+		float absX = Mathf.Abs(x);
 		animAdd = false;
+		float velocity = 0.0f;
 		if (currentAnimation != "wakeup_nosword")
 		{
-			float x = Input.GetAxis("Horizontal");
-			float absX = Mathf.Abs(x);
-			
 			//Debug.Log("x: " + x + "  absX: " + absX + "  Mathf.Sign(x):" + Mathf.Sign(x));
 			
 			if (x > 0)
@@ -43,7 +53,7 @@ public class cPlayer_c : MonoBehaviour
 			{
 				animLoop = true;
 				animationToPlay = "Runcycle_sword";
-				GetComponent<Rigidbody2D>().velocity = new Vector2(walkVelocity * Mathf.Sign(x), GetComponent<Rigidbody2D>().velocity.y);
+				velocity = 1.3f;
 			}
 			else if (absX > 0) 
 			{
@@ -53,8 +63,7 @@ public class cPlayer_c : MonoBehaviour
 					animLoop = false;
 					animationToPlay = "walkcycle_start_nosword";
 				}
-				
-				GetComponent<Rigidbody2D>().velocity = new Vector2(walkVelocity * Mathf.Sign(x), GetComponent<Rigidbody2D>().velocity.y);
+				velocity = 1f;
 			}
 			else
 			{
@@ -62,24 +71,63 @@ public class cPlayer_c : MonoBehaviour
 				{
 					animLoop = false;
 					animationToPlay = "walkcycle_end_nosword";
+					velocity = 0.3f;
 				}
 				else if (currentAnimation == "")
 				{
 					animLoop = true;
 					animationToPlay = "idle_nosword";
-					GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+					velocity = 0f;
 				}
 			}
 		}
+
+		movementDirection.x = velocity * walkVelocity * Mathf.Sign(x) * Time.deltaTime;
+		movementDirection.y = rb2D.velocity.y;
 	} //end - handleMovement
 
-	void handleJump()
+	void handleJump(float x)
 	{
 		if (Input.GetButtonDown ("jump"))
 		{
+			float absX = Mathf.Abs(x);
+
+			if (bGrounded)
+				iJumpCounter = 0;
+
+			iJumpCounter++;
+			if (iJumpCounter <= 2)
+				jumpDestHeight = rb2D.position.y+jumpHeight;
 			//animLoop = false;
 			//animationToPlay = "jump_sword";
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce));
+		}
+	}
+
+	void handleAttack()
+	{
+		if (Input.GetButtonDown ("attack")) 
+		{
+			animationToPlay = "attack_kick";
+			if (currentAnimation != "")
+				animationToPlayAfter = currentAnimation;
+			else
+				animationToPlayAfter = "idle_nosword";
+			animLoop = false;
+			animAdd = true;
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+			
+			GameObject turtle = GameObject.Find("turtle");
+			if (turtle == null)
+				return;
+			Vector2 enemyPos = turtle.transform.position;
+			Vector2 playerPos = this.gameObject.transform.position;
+			
+			float distance = Vector2.Distance (playerPos,enemyPos);
+			if (distance <= 3) 
+			{
+				turtle.SetActive(false);
+			}
+			
 		}
 	}
 
@@ -89,28 +137,82 @@ public class cPlayer_c : MonoBehaviour
 
 		skeletonAnimation.state.Start += startAnimListener;
 		skeletonAnimation.state.End += endAnimListener;
+
+		rb2D = GetComponent<Rigidbody2D>();
+		jumpDestHeight = -999.0f;
+		isAnimQueueDone = true;
 	}
-	
+
+	void FixedUpdate()
+	{
+
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
+		if (isAnimQueueDone == true) 
+		{
+			float x = Input.GetAxis("Horizontal");
+			
+			handleMovement (x);
+			handleJump (x);
 
-		handleMovement ();
-		handleJump ();
+			//movement
+			if (iJumpCounter >= 0)
+			{
+				//jump chunk
+				if (rb2D.position.y <= jumpDestHeight)
+				{
+					float  timePart = (jumpTime/1000.0f)/Time.deltaTime;
+					float jumpChunk = jumpHeight/timePart;
+					movementDirection.y += jumpChunk * Time.deltaTime;
+				}
+				else
+				{
+					jumpDestHeight = -999.0f;
+					if (bGrounded == false)
+						movementDirection.y -= gravity * Time.deltaTime;
+				}
+			}
+			Debug.Log( rb2D.position + movementDirection);
+			
+			rb2D.MovePosition( rb2D.position  + movementDirection);
 
-		Debug.Log(animationToPlay + "   " + animLoop);
-		SetAnimation (animationToPlay, animLoop);
+			handleAttack ();
+
+			if (animAdd) 
+			{
+				isAnimQueueDone = false;
+				if (animationToPlay != "")
+					skeletonAnimation.state.SetAnimation (0, animationToPlay, animLoop);
+				if (animationToPlayAfter != "")
+					skeletonAnimation.state.AddAnimation (0, animationToPlayAfter, true, 0);
+			} 
+			else
+				SetAnimation (animationToPlay, animLoop);
+		}
+		else
+			GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
 		
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Debug.Log("collision start");
+		bGrounded = true;
+		iJumpCounter = 0;
+		jumpDestHeight = -999.0f;
+
+		if (collision.gameObject.name == "groundGameEnd")
+		{
+			Application.LoadLevel("swordwhisperer");
+		}
 	}
 
 	void OnCollisionExit2D(Collision2D collision)
 	{
-		Debug.Log("collision exit");
+		bGrounded = false;
+		Debug.Log("exit");
 	}
 
 	//########################################
@@ -124,21 +226,37 @@ public class cPlayer_c : MonoBehaviour
 	void endAnimListener (Spine.AnimationState state, int trackIndex)
 	{
 		animIsPlaying = false;
-		currentAnimation ="";
-
-		if (state.GetCurrent (trackIndex).Animation.Name == "wakeup_nosword" || 
-		    state.GetCurrent (trackIndex).Animation.Name == "walkcycle_end_nosword")
+		if (isAnimQueueDone == false) 
 		{
-			animationToPlay = "idle_nosword";
-		}
-		else if (state.GetCurrent (trackIndex).Animation.Name == "walkcycle_start_nosword")
+			if (state.GetCurrent (trackIndex).Animation.Name == animationToPlayAfter)
+			{
+				isAnimQueueDone = true;
+				animAdd = false;
+				animationToPlay = "idle_nosword";
+				animationToPlayAfter = "";
+			}
+			
+		} 
+		else 
 		{
-			animationToPlay ="walkcycle_nosword";
+			if (state.GetCurrent (trackIndex).Animation.Name == "wakeup_nosword" || 
+			    state.GetCurrent (trackIndex).Animation.Name == "walkcycle_end_nosword")
+			{
+				animationToPlay = "idle_nosword";
+			}
+			else if (state.GetCurrent (trackIndex).Animation.Name == "walkcycle_start_nosword")
+			{
+				animationToPlay ="walkcycle_nosword";
+			}
+			currentAnimation ="";
 		}
 	}
 
 	void SetAnimation (string anim, bool loop) 
 	{
+		if (anim == "")
+			anim = "idle_nosword";
+
 		if (currentAnimation != anim) 
 		{
 			skeletonAnimation.state.SetAnimation(0, anim, loop);
---
+++ Assets/scripts/cTurtle.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine;
+
+[RequireComponent(typeof(SkeletonAnimation))]
+public class cTurtle : MonoBehaviour {
+
+	enum eTurtleMovement {ANIM_IDLE,ANIM_MOVE};
+
+	public SkeletonAnimation skeletonAnimation;
+	public float triggerDistance = 0.0f;
+	public float triggerAttack = 0.0f;
+
+	private string currentAnimation = "";
+	private string animationToPlay = "";
+	private bool animLoop = false;
+	private float currentTimeScale = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		skeletonAnimation.state.Start += startAnimListener;
+		skeletonAnimation.state.End += endAnimListener;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GameObject player = GameObject.Find("Player");
+		Vector2 playerPos = player.transform.position;
+		Vector2 enemyPos = this.gameObject.transform.position;
+
+		float distance = Vector2.Distance (enemyPos,playerPos);
+		if (distance <= triggerDistance) 
+		{
+			if (animationToPlay == "")
+			{
+				currentTimeScale = 0.7f;
+				animationToPlay = "wakeUP";
+				animLoop = false;
+			}
+			else if (animationToPlay != "wakeUP")
+			{
+				currentTimeScale = 1.0f;
+				animLoop = true;
+			}
+		}
+
+		SetAnimation (animationToPlay, animLoop);
+	}
+
+	//########################################
+	//################# Animation ############
+	//########################################
+	
+	void startAnimListener(Spine.AnimationState state, int trackIndex)
+	{
+
+	}
+	void endAnimListener (Spine.AnimationState state, int trackIndex)
+	{
+		currentAnimation ="";
+		//Debug.Log ("end - " + state.GetCurrent (trackIndex).Animation.Name);
+		if (state.GetCurrent (trackIndex).Animation.Name == "wakeUP")
+			animationToPlay = "Idle";
+	}
+	
+	void SetAnimation (string anim, bool loop) 
+	{
+		if (currentAnimation != anim) 
+		{	
+			skeletonAnimation.state.SetAnimation(0,anim,loop);
+			skeletonAnimation.timeScale = currentTimeScale;
+			currentAnimation = anim;
+		}
+	}
+}
